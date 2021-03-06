0af8fe536c7a2bb227bb391e2df4bbdddc14ac72
BooksCup <812809680@qq.com>
2021-03-06 16:07:43 +0800

    代码优化

--- app/src/main/java/com/wd/winddots/adapter/app/AppStoreAdapter.java
+++ app/src/main/java/com/wd/winddots/adapter/app/AppStoreAdapter.java
@@ -1,7 +1,12 @@
 package com.wd.winddots.adapter.app;
 
+import android.net.Uri;
+import android.text.TextUtils;
+
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.wd.winddots.R;
 import com.wd.winddots.entity.App;
 
 import java.util.List;
@@ -16,7 +21,15 @@ public class AppStoreAdapter extends BaseQuickAdapter<App, BaseViewHolder> {
 
     @Override
     protected void convert(BaseViewHolder helper, final App item) {
+        helper.setText(R.id.tv_name, item.getName())
+                .setText(R.id.tv_describe, item.getDescribes());
 
+        SimpleDraweeView mIconSdv = helper.getView(R.id.sdv_icon);
+        if (!TextUtils.isEmpty(item.getIcon())) {
+            mIconSdv.setImageURI(Uri.parse(item.getIcon()));
+        } else {
+            mIconSdv.setImageResource(R.mipmap.default_user_avatar);
+        }
     }
 
 }
