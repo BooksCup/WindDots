36eb9ba6111abc254477dfa4e4f699b4e2af0073
BooksCup <812809680@qq.com>
2021-03-19 16:37:39 +0800

    修复一个空指针BUG

--- app/src/main/java/com/wd/winddots/utils/StockUtil.java
+++ app/src/main/java/com/wd/winddots/utils/StockUtil.java
@@ -9,14 +9,16 @@ import java.util.List;
 public class StockUtil {
     public static List<GoodsSpec> getGoodsSpecListFromStockRecordList(List<StockApplicationInRecord> stockApplicationInRecordList) {
         List<GoodsSpec> goodsSpecList = new ArrayList<>();
-        for (StockApplicationInRecord stockApplicationInRecord : stockApplicationInRecordList) {
-            GoodsSpec goodsSpec = new GoodsSpec();
-            goodsSpec.setId(stockApplicationInRecord.getGoodsSpecId());
-            goodsSpec.setX(stockApplicationInRecord.getX());
-            goodsSpec.setY(stockApplicationInRecord.getY());
-            goodsSpec.setApplyNum(stockApplicationInRecord.getApplyNumber());
-            goodsSpec.setNum(stockApplicationInRecord.getCount());
-            goodsSpecList.add(goodsSpec);
+        if (null != stockApplicationInRecordList) {
+            for (StockApplicationInRecord stockApplicationInRecord : stockApplicationInRecordList) {
+                GoodsSpec goodsSpec = new GoodsSpec();
+                goodsSpec.setId(stockApplicationInRecord.getGoodsSpecId());
+                goodsSpec.setX(stockApplicationInRecord.getX());
+                goodsSpec.setY(stockApplicationInRecord.getY());
+                goodsSpec.setApplyNum(stockApplicationInRecord.getApplyNumber());
+                goodsSpec.setNum(stockApplicationInRecord.getCount());
+                goodsSpecList.add(goodsSpec);
+            }
         }
         return goodsSpecList;
     }
